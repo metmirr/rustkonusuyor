a8933dba5a4be2070ff2891ed29b1c1128c6c217
Metin Demir <metmirr@users.noreply.github.com>
2016-11-10 14:44:56 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,5 +1,16 @@
+/// Değişken tanımlama:
+///   - Değişken tanımlarken `let` anahtar kelimesi kullanılır. Değerleri 
+///   değiştirilebilir değişkenler `let mut` şeklinde tanımlanmalıdır(mut : mutable ).
+///   - Değişken değerleri ekrana yazdırılırken tırnaklar arasına {} işaretinin eklenmesi gerekir.
+///   aksi halde değişken değeri ekrana yazdırılmaz.
 
 fn main() {
-  println!("Merhaba Dünya");
+  let x = 100;
+  //x = 200; //hata! 
+  let mut y = 300;
+  //y = 400; //çalışır
+  
+  // x değişkeninin değerini ekrana yazdırma.
+  println!("{}", x);
 }
 
