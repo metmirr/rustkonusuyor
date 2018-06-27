f55f78afe9a3f4d79e9740b62ac38502926768e8
metmirr <metin7087@gmail.com>
2018-06-28 00:35:04 +0300

    Projenin kolay yönetimi için cargo destegi eklendi

--- ornekler/fonksiyonlar/fonksiyonlar.rs
+++ ornekler/fonksiyonlar/fonksiyonlar.rs
@@ -9,20 +9,41 @@
 // yazilip noktali virgul konulmazsa return kelimesine gerek yoktur. Cogunlukla kullanilan bir 
 // tasarimdir.
 //
+// Fonksiyonlar degiskenlere atanan degerler gibi kullanilabilirler
+// 
+//   let f: f();
+//
+// Bu sekilde tanimlanan bir degiskene ancak bir fonksiyon atanabilir, cunku degiskenin tipi 
+// fonksiyon tipindedir, ve atanan fonksiyonun imzasi(parametreler, geri donus degeri) uyusmalidir.
+
 
 fn main() {
 	let x = toplama(1, 3);
 	println!("{}", x);
 	println!("{}", carpma(4, 5));
-	bolme(5.0, 9.0)
+	bolme(5.0, 9.0);
+
+    let f: fn(); // parametresiz, geri donus degeri yok
+    let f1: fn(i32, f32);
+
+    let f3: fn(&str) = sayhello;
+    f3("Suleyman");
+
 }
 
 fn toplama(a: i32, b: i32) -> i32 {
 	a + b // geri donus degeri
 }
+
 fn carpma(a: i32, b: i32) -> i32 {
 	return a * b;
 }
+
 fn bolme(a: f32, b: f32) {
 	println!("{}", (a / b));
 }
+
+fn sayhello(name: &str) {
+    println!("Merhaba, {}!", name);
+}
+
